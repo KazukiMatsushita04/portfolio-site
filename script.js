780f0e5e8c2f58cfd8a1d7849695d73688b35bd2
shikoku-km <62467360+shikoku-km@users.noreply.github.com>
2020-08-15 19:11:34 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//コンテンツがゆっくり出てくる
+const targetElement = document.querySelectorAll(".animationTarget");
+console.log(targetElement);
+document.addEventListener("scroll", function () {
+    for (let i = 0; i < targetElement.length; i++) {
+        const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * .2;
+        if (window.innerHeight > getElementDistance) {
+            targetElement[i].classList.add("show");
+        }
+    }
+})
+//ページトップに戻る
+$(document).ready(function () {
+    var pagetop = $(".pagetop");
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+          pagetop.fadeIn();
+        } else {
+          pagetop.fadeOut();
+        }
+      });
+    pagetop.click(function () {
+      $("body, html").animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+  });
