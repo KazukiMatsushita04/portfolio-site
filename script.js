d96268730b487c33c1031ee7e429d4accafd65e5
KazukiMatsushita <62467360+KazukiMatsushita04@users.noreply.github.com>
2020-09-08 22:04:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ $(document).ready(function () {
       return false;
     });
   });
+  //トップイメージ
+  var particles = Particles.init({
+    selector: '.background',
+  sizeVariations: 10,
+  color: ['#00bbdd', '#404B69', '#DBEDF3'],
+  connectParticles: true
+});
